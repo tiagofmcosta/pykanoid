3778f4fd0fd0f3f43de830a6a26d44f809ef8f78
Tiago Costa <costa.tiago@gmail.com>
2024-04-10 22:24:30 +0200

    Change default debug text Y
    * Set to the bottom of the screen to improve visibility

--- pykanoid/debug.py
+++ pykanoid/debug.py
@@ -1,11 +1,13 @@
 import pygame
 
+from pykanoid.settings import WINDOW_HEIGHT
+
 pygame.init()
 
 __font = pygame.font.Font(None, 30)
 
 
-def debug(info, y=10, x=10):
+def debug(info, y=WINDOW_HEIGHT - 30, x=10):
     display_surface = pygame.display.get_surface()
     debug_surface = __font.render(str(info), True, "White")
     debug_rect = debug_surface.get_rect(topleft=(x, y))
